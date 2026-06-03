a4f4a9018682624eb8365e341738322fe8031960
RioRivaldy <101505394+RioRivaldy@users.noreply.github.com>
2026-06-03 10:49:22 +0700

    Update smarthos_app.py

--- smarthos_app.py
+++ smarthos_app.py
@@ -6,6 +6,6 @@ import os
 
 st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
 
-with st.container():
-    st.write("Text inside the container")
-    st.button("A button inside the container")
+st.markdown("""
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
